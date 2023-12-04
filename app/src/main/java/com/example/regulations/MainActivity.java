c84dca239e3a0f8006713c698bf63157c777d1e2
Hill <125409221+NARI1108@users.noreply.github.com>
2023-12-04 18:21:17 +0330

    I deleted some extra methods and added a series of views needed for this activity. And I created a method to click on the buttons created in the app's home section.

--- app/src/main/java/com/example/regulations/MainActivity.java
+++ app/src/main/java/com/example/regulations/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.regulations;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
@@ -10,72 +11,54 @@ import android.os.Handler;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
-    CoordinatorLayout coordinatorLayout;
-    TextView txt_time;
-    int time=1200000;
+    Button btn_Exam, btn_Education, btn_contact, btn_about;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
-        snackBar();
-        timer();
+        onClick();
     }
 //    This method is for connecting views from the XML layer to this layer.
     public void findViews(){
-        coordinatorLayout = findViewById(R.id.coordinator);
-        txt_time = findViewById(R.id.txt_time);
+      btn_Exam = findViewById(R.id.btn_exam);
+      btn_Education = findViewById(R.id.btn_Education);
+      btn_contact = findViewById(R.id.btn_contact);
     }
-//    This method is for a box at the bottom of the app that represents the snack bar.
-    public void snackBar(){
-        String result ="این یک متن تستی است.", text = "تلاش دوباره";
-        Snackbar mysnackbar = Snackbar.make(coordinatorLayout,result,Snackbar.LENGTH_SHORT);
-        mysnackbar.setDuration(15000);
-        mysnackbar.show();
-        mysnackbar.setAction(text, new View.OnClickListener() {
+//  This method is for the user to click on the buttons in the home section of the app.
+    public void onClick(){
+        btn_Exam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-               finish();
+                startActivity(new Intent(MainActivity.this,Exam.class));
             }
         });
-        View snack_View = mysnackbar.getView();
-        snack_View.setBackgroundColor(getResources().getColor(R.color.orange));
-        TextView textView = snack_View.findViewById(android.support.design.R.id.snackbar_text);
-        textView.setTextColor(Color.BLUE);
-    }
-//    This method is for counting time.
-    private void timer(){
-        String result ="زمان به پایان رسید";
-        time-=1000;
-        txt_time.setText(formatTime(time));
-        new Handler().postDelayed(new Runnable() {
+        btn_Education.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                if(time == 0){
-                      Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
-                }else {
-                    timer();
-                }
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,Education.class));
             }
-        }, 1000);
-    }
-    private String formatTime(int time){
-        int second = (time/1000);
-        int minute = (second/60);
-        second%=60;
-        return "زمان باقیمانده:" + String.format(Locale.ENGLISH, "%02d", second) + " : " + String.format(Locale.ENGLISH,"%20d",minute);
-    }
-    public void btnClick(View view){
-
-      Intent msg = new Intent(Intent.ACTION_VIEW);
-      msg.setData(Uri.parse("https://telegram.com/Nr_Ai80"));
-      startActivity(msg);
+        });
+        btn_contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent message = new Intent(Intent.ACTION_VIEW);
+                message.setData(Uri.parse("https://t.me/Nr_Ai80"));
+                startActivity(message);
+            }
+        });
+        btn_about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this).setTitle(getResources().getString(R.string.about_us)).setMessage(getResources().getString(R.string.about_company)).show();
+            }
+        });
     }
 }
